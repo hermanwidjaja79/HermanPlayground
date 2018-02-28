f71a9c2bd2982633a41f050ceb79ada6bfc3981d
hermanwidjaja79 <36845599+hermanwidjaja79@users.noreply.github.com>
2018-02-28 10:57:31 +0700

    Initail implementation of FileHashChecker

---
+++ FileHashChecker/FileHashChecker/FileHashCheckerLib.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace FileHashChecker
+{
+    public class FileHashCheckerLib
+    {
+        public static string GetHashCodeFromFile(string strFilename)
+        {
+            string strHashPassword = String.Empty; 
+            if (strFilename is null || !File.Exists(strFilename))
+            {
+                throw new IOException("GetHashCodeFromFile() - Invalid \"strFilename\" parameter");
+            }
+
+            using (var stream = File.OpenRead(strFilename))
+            {
+                return strHashPassword = GetHashCodeFromFile(stream);
+            }
+        }
+
+        public static string GetHashCodeFromFile(Stream stream)
+        {
+            if (stream is null)
+            {
+                throw new IOException("GetHashCodeFromFile() - Invalid \"stream\" parameter");
+            }
+
+            MD5 md5Instance = MD5.Create();
+            byte[] hash = md5Instance.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+    }
+}
---
+++ FileHashChecker/FileHashCheckerTests/FileHashCheckerLibTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileHashChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHashChecker.Tests
+{
+    [TestClass()]
+    public class FileHashCheckerLibTests
+    {
+        #region HelperFunctions
+        // Helper Functions
+        //
+        protected Dictionary<string, string> PrepareTestMD5Library()
+        {
+            Dictionary<string, string> lstMD5TestLibrary = new Dictionary<string, string>();
+            lstMD5TestLibrary.Add(
+                "This is a temporary file. Created by FileHashCheckerLib Tests on " + DateTime.Now.ToShortDateString(),
+                "8139C7BA321ED5293F03A02D760D7A4A"
+                );
+            return lstMD5TestLibrary;
+        }
+        #endregion
+
+        [TestMethod()]
+        public void GetHashCodeFromFileTest()
+        {
+            Dictionary<string, string> dctTestFiles = PrepareTestMD5Library();
+            foreach (KeyValuePair<string, string> entry in dctTestFiles)
+            {
+                // Create temp file
+                //
+                string strTempFileName = System.IO.Path.GetTempFileName();
+                File.AppendAllText(strTempFileName, entry.Key);
+
+                Assert.AreEqual(entry.Value, FileHashCheckerLib.GetHashCodeFromFile(strTempFileName));
+
+                // clear out temp file
+                //
+                File.Delete(strTempFileName);
+            }
+        }
+    }
+}
